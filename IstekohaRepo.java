b3f7697b643949401fceb51588b861f4fe20cb68
kertuceitlyn <144665784+kertuceitlyn@users.noreply.github.com>
2025-03-31 14:59:35 +0300

    uuendus

--- IstekohaRepo.java
+++ IstekohaRepo.java
@@ -3,8 +3,7 @@ package com.example.demo_fl;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface IstekohaRepo extends JpaRepository<Istekoht, Long> {
-    List<Istekoht> findByLendAndKasHõivatudFalse(Lend lend);
+    List<Istekoht> findByLendAndKashõivatudFalse(Lend lend);
 }
