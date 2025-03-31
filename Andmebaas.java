85d62cf1ae780653841f100283b994f3ac59097b
kertuceitlyn <144665784+kertuceitlyn@users.noreply.github.com>
2025-03-31 14:59:00 +0300

    Klass eemaldatud

--- Andmebaas.java
+++ Andmebaas.java
@@ -1,4 +1,4 @@
-package com.example.demo_fl;
+/*package com.example.demo_fl;
 
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
@@ -16,3 +16,4 @@ public class Andmebaas {
                 .build();
     }
 }
+*/
